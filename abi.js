9f4ae17ddbf4e489cc3dfdcb8683ad2b8a20e66a
Yuet Wong <contact@yuetloo.com>
2018-04-07 16:17:38 -0400

    added ethers.io library

---
+++ abi.js
@@ -0,0 +1,37 @@
+var abi = [
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "Claim",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "gender",
+				"type": "uint8"
+			},
+			{
+				"name": "age",
+				"type": "uint8"
+			},
+			{
+				"name": "smoke",
+				"type": "bool"
+			},
+			{
+				"name": "country",
+				"type": "string"
+			}
+		],
+		"name": "Register",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
